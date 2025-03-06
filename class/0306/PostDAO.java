b569961ad105b708db7a1681225a1fea9dd4e358
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-03-06 14:58:54 +0900

    Add files via upload

---
+++ class/0306/PostDAO.java
@@ -0,0 +1,27 @@
+package siat.study.post.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import siat.study.post.domain.PostRequestDTO;
+import siat.study.post.domain.PostResponseDTO;
+
+// xxxDAO(Data Access Object)로 DBMS와 작업을 전담하는 역할
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDAO {
+    
+    private PostRequestDTO[] requestAry  ;  
+    private int idx ; 
+
+    public void insertRow(PostRequestDTO request) {
+        requestAry[idx++] = request ; 
+    }
+
+    public PostRequestDTO[] selectRow() {
+        return requestAry ; 
+    }
+
+}
